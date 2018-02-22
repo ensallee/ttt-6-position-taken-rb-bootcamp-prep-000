04421982a86ac511f7e8f511b1c62bf103791cb9
Betsy Sallee <ensallee@gmail.com>
2018-02-22 18:18:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-
+  if index == " "
+    false
+  elsif index == ""
+    false
+  elsif index == nil
+    false
+  else
+    true
 end
